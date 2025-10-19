6a193343e66af72aa7f3c5c70f4ae040d8b945a6
jlouissaint13 <114112722+jlouissaint13@users.noreply.github.com>
2025-10-19 18:44:57 -0400

    working on pdf manager

--- frontend/electron/main.ts
+++ frontend/electron/main.ts
@@ -27,8 +27,7 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win: BrowserWindow | null
 
 function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+  win = new BrowserWindow({icon: path.join(process.env.VITE_PUBLIC, 'marie-icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
